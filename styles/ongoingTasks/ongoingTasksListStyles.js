87f24dea1cd9db385f086cfb12b285dd16da1e9e
favour <fackonu@st.ug.edu.gh>
2024-06-02 21:57:20 +0000

    Fix wrong naming

--- styles/ongoingTasks/ongoingTasksListStyles.js
+++ styles/ongoingTasks/ongoingTasksListStyles.js
@@ -1,10 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 export const ongoingTasksListStyles = StyleSheet.create({
-    ongoingTasksContainer: {
-        display: 'flex', 
-        flexDirection: 'column',
-        height: 180,
-        marginTop: 10,
-    },
+    ongoingTasksListContainer: {
+        
+    }
 })
