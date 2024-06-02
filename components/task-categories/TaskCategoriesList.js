adb8739863fb1b05a1fd404d9fa647bc2548a0f4
favour <fackonu@st.ug.edu.gh>
2024-06-02 23:03:27 +0000

    Add more categories to list of categories

--- components/task-categories/TaskCategoriesList.js
+++ components/task-categories/TaskCategoriesList.js
@@ -5,6 +5,9 @@ import workImage from '../../assets/work-category.png';
 import exerciseImage from '../../assets/exercise-category.png';
 import travelImage from '../../assets/travel-category.png';
 import hobbiesImage from '../../assets/hobbies-category.png';
+import cookingImage from '../../assets/cooking-category.png';
+import programmingImage from '../../assets/programming-category.png';
+import goalsImage from '../../assets/goals-category.png';
 
 import { taskCategoriesListStyles } from '../../styles/taskCategoriesStyles/taskCategoriesListStyles';
 
@@ -15,6 +18,9 @@ export default function TaskCategoriesList(){
         { id: 3, title: 'Work', numberOfTasks: 6, photo: workImage },
         { id: 4, title: 'Travel', numberOfTasks: 8, photo: travelImage },
         { id: 5, title: 'Hobbies', numberOfTasks: 8, photo: hobbiesImage },
+        { id: 6, title: 'Cooking', numberOfTasks: 3, photo: cookingImage },
+        { id: 7, title: 'Programming', numberOfTasks: 4, photo: programmingImage },
+        { id: 8, title: 'Goals', numberOfTasks: 7, photo: goalsImage },
     ];
     return(
         <FlatList horizontal showsHorizontalScrollIndicator = {false} data = {categories} style = {taskCategoriesListStyles.taskCategoryListContainer}
