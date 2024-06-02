9725d488149b39c92b59c26c1d8979bd53c5474f
favour <fackonu@st.ug.edu.gh>
2024-06-02 17:59:40 +0000

    Use SearchBox and SearchFilterButton in Search

--- components/search/Search.js
+++ components/search/Search.js
@@ -1,9 +1,13 @@
 import { View } from 'react-native';
+import SearchBox from './SearchBox';
+import SearchFilterButton from './SearchFilterButton';
 import { searchStyles } from '../../styles/searchStyles/searchStyles';
 
 export default function Search(){
     return(
         <View style = {searchStyles.searchContainer}>
+            <SearchBox/>
+            <SearchFilterButton/>
         </View>
     )
 }
