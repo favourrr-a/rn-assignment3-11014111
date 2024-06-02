9896101cd081f636df326c3be23d8b7ba94a52ef
favour <fackonu@st.ug.edu.gh>
2024-06-02 22:30:21 +0000

    Add more tasks to list of tasks

--- components/ongoing-tasks/OngoingTasksList.js
+++ components/ongoing-tasks/OngoingTasksList.js
@@ -4,11 +4,18 @@ import { ongoingTasksListStyles } from '../../styles/ongoingTasks/ongoingTasksLi
 
 export default function OngoingTasksList(){
     const tasks = [
-        { id: 1, title: 'Mobile App Development' },
-        { id: 2, title: 'Web development' },
-        { id: 3, title: 'Pushups' },
-        { id: 4, title: 'DCIT202 assignment' },
-        { id: 5, title: 'Reading' },
+        { id: 1, title: 'Practice Mobile App Development' },
+        { id: 2, title: 'Improve Web development skills' },
+        { id: 3, title: 'Do Pushups' },
+        { id: 4, title: 'Do DCIT202 assignment' },
+        { id: 5, title: 'Study MATH220' },
+        { id: 6, title: 'Come up with ideas for upcoming projects' },
+        { id: 7, title: 'Listen to uplifting music' },
+        { id: 8, title: 'Practice gratitude' },
+        { id: 9, title: 'Work on python project' },
+        { id: 10, title: 'Borrow book at library' },
+        { id: 11, title: 'Read The Idiot by Fyodor Dostoevsky' },
+        { id: 12, title: 'Contemplate life' },
     ];
     return(
         <View style = {ongoingTasksListStyles.ongoingTasksListContainer}>
