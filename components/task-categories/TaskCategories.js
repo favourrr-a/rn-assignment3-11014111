86e5bbb7f198d949ae0c22ae7caa78134928a3de
favour <fackonu@st.ug.edu.gh>
2024-06-02 19:06:06 +0000

    Add 'Categories' title

--- components/task-categories/TaskCategories.js
+++ components/task-categories/TaskCategories.js
@@ -1,10 +1,12 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { taskCategoriesStyles } from '../../styles/taskCategoriesStyles/taskCategoriesStyles';
 
 export default function TaskCategories(){
     return(
         <View style = {taskCategoriesStyles.taskCategoriesContainer}>
-
+            <Text style = {taskCategoriesStyles.taskCategoriesTitle}>
+                Categories
+            </Text>
         </View>
     )
 }
--- styles/taskCategoriesStyles/taskCategoriesStyles.js
+++ styles/taskCategoriesStyles/taskCategoriesStyles.js
@@ -3,5 +3,9 @@ import { StyleSheet } from 'react-native';
 export const taskCategoriesStyles = StyleSheet.create({
     taskCategoriesContainer: {
         marginTop: 30,
+    },
+    taskCategoriesTitle: {
+        fontSize: 23,
+        fontWeight: 'bold',
     }
 })
