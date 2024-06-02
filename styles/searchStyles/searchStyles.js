aa84edbbea151a48dd4f1e0b5a4eb7727baa7ba3
favour <fackonu@st.ug.edu.gh>
2024-06-02 18:04:17 +0000

    Align SearchBox and SearchFilterButton in a row

--- styles/searchStyles/searchStyles.js
+++ styles/searchStyles/searchStyles.js
@@ -4,5 +4,8 @@ export const searchStyles = StyleSheet.create({
     searchContainer: {
         width: '100%',
         marginTop: 20,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
     },
 })
